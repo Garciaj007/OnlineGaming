dcfb106c20a69f616385cddc87cea56dc506bebd
Juriel Garcia <jurielgarcia2010@gmail.com>
2018-04-20 00:23:31 -0400

    1.0.2

--- Semester 2/Mini Assignment/script.js
+++ Semester 2/Mini Assignment/script.js
@@ -10,13 +10,19 @@ const WIDTH = 800,
 var canvas, ctx, intervalID, player, r_Anim, ramona;
 
 var controller = {
-    left: false,
-    right: false,
-    jump: false,
-    punch: false,
-    kick: false,
-    tech: false,
-    guard: false
+    action: {
+        idle: true,
+        walk: false,
+        jump: false,
+        punch: false,
+        kick: false,
+        tech: false,
+        guard: false,
+    },
+    direction:{
+        left: false,
+        right: false
+    }
 }
 
 var ramona = {
@@ -31,8 +37,8 @@ var ramona = {
     ramonaKickLeft: new Sprite("Sprites/Ramona_Kick_Left.png", 5, 8, false, new Vector(300, 70)),
     ramonaGuardRight: new Sprite("Sprites/Ramona_Guard_Right.png", 5, 8, false, new Vector(250, 70)),
     ramonaGuardLeft: new Sprite("Sprites/Ramona_Guard_Left.png", 5, 8, false, new Vector(250, 70)),
-    ramonaTechRight: new Sprite("Sprites/Ramona_Tech.png", 13, 8, false, new Vector(1170, 80)),
-    ramonaTechLeft: new Sprite("Sprites/Ramona_Tech.png", 13, 8, false, new Vector(1170, 80))
+    ramonaTechRight: new Sprite("Sprites/Ramona_Tech_Right.png", 13, 8, false, new Vector(1170, 80)),
+    ramonaTechLeft: new Sprite("Sprites/Ramona_Tech_Left.png", 13, 8, false, new Vector(1170, 80))
 }
 
 function init() {
@@ -41,28 +47,41 @@ function init() {
     canvas.width = WIDTH;
     canvas.height = HEIGHT;
 
-    r_Anim = new Animation(ramona.ramonaIdle);
-    player = new Player(r_Anim, new Vector(WIDTH / 2, HEIGHT / 2));
+    player = new Player(ramona.ramonaIdle);
+    player.rigidbody.gravity.set(0, 0.001);
+    player.rigidbody.mass = 1.0;
+    player.rigidbody.position.set(WIDTH / 2, HEIGHT / 2);
 
     intervalID = setInterval(Render, 16);
 }
 
 function Render() {
     clearCanvas();
-
-    if (controller.left) {
-        player.animation.set(ramona.ramonaWalkingLeft);
-        player.velocity.x = -1;
-    } else if (controller.right) {
-        player.animation.set(ramona.ramonaWalkingRight);
-        player.velocity.x = 1;
+    
+    if(controller.direction.left){
+        player.rigidbody.velocity.x = -1;
+        if(controller.action.walk){
+            player.animation.set(ramona.ramonaWalkingLeft);
+        }
+        if(controller.action.tech){
+            console.log("tech used");
+            player.animation.set(ramona.ramonaTechLeft);
+        }
+    } else if(controller.direction.right){
+        player.rigidbody.velocity.x = 1;
+        if(controller.action.walk){
+            player.animation.set(ramona.ramonaWalkingRight);
+        }
+        if(controller.action.tech){
+            player.animation.set(ramona.ramonaTechRight);
+        }
     } else {
-        player.animation.set(ramona.ramonaIdle);
-        player.velocity.x = 0;
+        player.rigidbody.velocity.x = 0;
+        if(controller.action.idle){
+            player.animation.set(ramona.ramonaIdle);
+        }
     }
-
-    player.animation.draw();
-    player.animation.update();
+    
     player.update();
 }
 
@@ -76,7 +95,15 @@ function Sprite(_url, _numFrames, _tickFrames, _loop, _size, _position) {
     this.position = _position || new Vector();
 }
 
-function Animation(sprite) {
+function RigidBody(){
+    this.mass = 0.0;
+    this.position = new Vector();
+    this.velocity = new Vector();
+    this.acceleration = new Vector();
+    this.gravity = new Vector(0, 9.8);
+}
+
+function Animation(sprite = new Sprite()){
     this.frameIndex = 0;
     this.tickCount = 0;
     this.ticksPerFrame = sprite.ticksPerFrame;
@@ -88,16 +115,9 @@ function Animation(sprite) {
     this.image.src = sprite.URL;
 }
 
-function Player(animation, initialPosition) {
-    if (arguments.length == 1) {
-        this.animation = animation || null;
-        this.animation.position = new Vector();
-    } else if (arguments.length == 2) {
-        this.animation = animation || null;
-        this.animation.position = initialPosition || new Vector();
-    }
-    this.velocity = new Vector();
-    this.acceleration = new Vector();
+function Player(initSprite) {
+        this.animation = new Animation(initSprite);
+        this.rigidbody = new RigidBody();
 }
 
 function Vector(x = 0, y = 0) {
@@ -115,6 +135,7 @@ Animation.prototype.update = function () {
             if (this.frameIndex < this.numOfFrames - 1) {
                 this.frameIndex += 1;
             } else if (this.loop) {
+                console.log("Oneshot done...");
                 this.frameIndex = 0;
             }
         }
@@ -125,7 +146,7 @@ Animation.prototype.update = function () {
     }
 }
 
-Animation.prototype.draw = function (position = new Vector(0, 0)) {
+Animation.prototype.draw = function (position = new Vector()) {
     //ctx.drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight);
     ctx.drawImage(this.image, this.frameIndex * this.size.x / this.numOfFrames, 0, this.size.x / this.numOfFrames, this.size.y, this.position.x, this.position.y, this.size.x / this.numOfFrames, this.size.y);
 }
@@ -144,23 +165,40 @@ Animation.prototype.reset = function () {
     this.tickCount = 0;
 }
 
-Player.prototype.applyForce = function (force) {
-    this.acceleration.add(force);
+RigidBody.prototype.update = function(){
+    this.position.add(this.velocity);
+    this.velocity.add(this.acceleration);
+    this.acceleration.add(this.gravity);
+    
+    if(this.position.y > HEIGHT - 100){
+        this.velocity.y = 0;
+    }
+}
+
+RigidBody.prototype.applyForce = function(force = new Vector()){
+    this.acceleration.add(force.div(this.mass));
 }
 
 Player.prototype.update = function () {
-    this.animation.position.add(this.velocity);
-    this.velocity.add(this.acceleration);
-}
-
-Vector.prototype.add = function (v = Vector(0, 0)) {
-    this.x += v.x;
-    this.y += v.y;
+    this.animation.position = this.rigidbody.position;
+    this.animation.draw();
+    this.animation.update();
+    this.rigidbody.update();
 }
 
 Vector.prototype.set = function(x = 0, y = 0){
     this.x = x;
     this.y = y;
+}
+
+Vector.prototype.add = function (v = new Vector()) {
+    this.x += v.x;
+    this.y += v.y;
+}
+
+Vector.prototype.div = function(d){
+    this.x = this.x / d;
+    this.y = this.y / d;
 }
 
 //Helper Functions
@@ -170,23 +208,40 @@ function clearCanvas() {
 }
 
 function keydownHandler(e) {
+    controller.action.idle = false;
     switch (e.code) {
         case "KeyA":
-            controller.left = true;
+            controller.direction.left = true;
+            controller.action.walk = true;
             break;
         case "KeyD":
-            controller.right = true;
+            controller.direction.right = true;
+            controller.action.walk = true;
+            break;
+        case "Space":
+            controller.action.walk = false;
+            controller.action.jump = true;
+            break;
+        case "KeyQ":
+            controller.action.walk = false;
+            controller.action.tech = true;
             break;
     }
 }
 
 function keyupHandler(e) {
+    controller.action.idle = true;
     switch (e.code) {
         case "KeyA":
-            controller.left = false;
+            controller.direction.left = false;
+            controller.action.walk = false;
             break;
         case "KeyD":
-            controller.right = false;
+            controller.direction.right = false;
+            controller.action.walk = false;
+            break;
+        case "Space": 
+            controller.action.jump = false;
             break;
     }
 }
